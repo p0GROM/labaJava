de3ac3bba82fad2f2d84219029a7bfbad58e7af0
Egor <samasobakka@gmail.com>
2024-10-27 15:19:15 +0300

    Добавлен класс Repository и выполнен рефакторинг

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -1,67 +1,118 @@
-import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+    // Класс, представляющий автомобиль
     static class Car {
-        String carBrand;
-        String carClass;
-        float weight;
-        Driver driver;
-        Engine engine;
+        String carBrand; // Марка автомобиля
+        String carClass; // Класс автомобиля
+        float weight; // Вес автомобиля
+        Driver driver; // Водитель, связанный с автомобилем
+        Engine engine; // Двигатель, связанный с автомобилем
 
+        // Вложенный класс, представляющий водителя
         static class Driver {
-            String name;
-            int driverExp;
+            String name; // Имя водителя
+            int driverExp; // Стаж водителя в годах
         }
 
+        // Вложенный класс, представляющий двигатель
         static class Engine {
-            String maker;
-            int power;
+            String maker; // Производитель двигателя
+            int power; // Мощность двигателя
         }
 
+        // Конструктор инициализирует водителя и двигатель
         public Car() {
             this.driver = new Driver();
             this.engine = new Engine();
         }
 
-        public static void outputDriverInfo(int count, Car[] cars) {
-            for (int i = 0; i < count; i++) {
-                System.out.println();
-                System.out.println("Водитель #" + (i + 1));
-                System.out.println("ФИО водителя: " + cars[i].driver.name);
-                System.out.println("Водительский стаж: " + cars[i].driver.driverExp);
-                System.out.println("Марка машины: " + cars[i].carBrand);
-                System.out.println("Класс машины: " + cars[i].carClass);
-                System.out.println("Вес машины: " + cars[i].weight);
-                System.out.println("Производитель двигателя: " + cars[i].engine.maker);
-                System.out.println("Мощность двигателя: " + cars[i].engine.power);
-                System.out.println();
+        // Метод для вывода информации о водителе и деталях автомобиля
+        public static void outputDriverInfo(Car car) {
+            System.out.println();
+            System.out.println("ФИО водителя: " + car.driver.name);
+            System.out.println("Водительский стаж: " + car.driver.driverExp);
+            System.out.println("Марка машины: " + car.carBrand);
+            System.out.println("Класс машины: " + car.carClass);
+            System.out.println("Вес машины: " + car.weight);
+            System.out.println("Производитель двигателя: " + car.engine.maker);
+            System.out.println("Мощность двигателя: " + car.engine.power);
+            System.out.println();
+        }
+
+        // Метод для вывода информации о всех водителях
+        public static void outputAllDrivers(List<Car> cars) {
+            for (Car car : cars) {
+                outputDriverInfo(car);
             }
         }
 
-        public static void outputDriverThan5Years(int count, Car[] cars) {
-            for (int i = 0; i < count; i++) {
-                if (cars[i].driver.driverExp >= 5) {
-                    outputDriverInfo(1, new Car[]{cars[i]});
+        // Метод для вывода информации о водителях со стажем более 5 лет
+        public static void outputDriverThan5Years(List<Car> cars) {
+            for (Car car : cars) {
+                if (car.driver.driverExp >= 5) {
+                    outputDriverInfo(car);
                 }
             }
         }
 
-        public static void outputDriverSovietCar(int count, Car[] cars) {
-            for (int i = 0; i < count; i++) {
-                if (cars[i].engine.maker.equals("СССР")) {
-                    outputDriverInfo(1, new Car[]{cars[i]});
+        // Метод для вывода информации о водителях с советскими автомобилями
+        public static void outputDriverSovietCar(List<Car> cars) {
+            for (Car car : cars) {
+                if (car.engine.maker.equals("СССР")) {
+                    outputDriverInfo(car);
                 }
             }
         }
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Car[] cars = new Car[10];
+    // Класс для управления репозиторием автомобилей
+    static class CarRepository {
+        private List<Car> cars; // Список для хранения автомобилей
 
-        int choice = 0;
-        int i = 0;
+        // Конструктор инициализирует список автомобилей
+        public CarRepository() {
+            this.cars = new ArrayList<>();
+        }
+
+        // Метод для добавления автомобиля в репозиторий
+        public void addCar(Car car) {
+            cars.add(car);
+        }
+
+        // Метод для удаления автомобиля по индексу
+        public void removeCar(int index) {
+            if (index >= 0 && index < cars.size()) {
+                cars.remove(index);
+            } else {
+                System.out.println("Некорректный индекс.");
+            }
+        }
+
+        // Метод для обновления автомобиля по индексу
+        public void updateCar(int index, Car car) {
+            if (index >= 0 && index < cars.size()) {
+                cars.set(index, car);
+            } else {
+                System.out.println("Некорректный индекс.");
+            }
+        }
+
+        // Метод для получения списка автомобилей
+        public List<Car> getCars() {
+            return cars;
+        }
+    }
+
+    // Основной метод для запуска приложения
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Сканер для ввода пользователя
+        CarRepository carRepository = new CarRepository(); // Создание репозитория автомобилей
+
+        int choice = 0; // Переменная для хранения выбора меню пользователя
         do {
             System.out.println();
             System.out.println("------------Меню------------");
@@ -69,58 +120,59 @@ public class Main {
             System.out.println("2. Просмотреть все записи");
             System.out.println("3. Просмотреть записи водителей со стажем более 5 лет");
             System.out.println("4. Просмотреть записи водителей с советскими автомобилями");
-            System.out.println("5. Выход");
-            System.out.print("Выберите действие от 1 до 5: ");
+            System.out.println("5. Удалить запись");
+            System.out.println("6. Выход");
+            System.out.print("Выберите действие от 1 до 6: ");
             try {
-                choice = scanner.nextInt();
-                scanner.nextLine();
+                choice = scanner.nextInt(); // Получение выбора пользователя
+                scanner.nextLine(); // Потребление символа новой строки
                 switch (choice) {
-                    case 1:
-                        if (i < cars.length) {
-                            cars[i] = new Car();
-                            System.out.println();
-                            System.out.print("Введите ФИО водителя " + (i + 1) + ": ");
-                            cars[i].driver.name = scanner.nextLine();
-                            System.out.print("Введите стаж водителя " + (i + 1) + ": ");
-                            cars[i].driver.driverExp = scanner.nextInt();
-                            scanner.nextLine();
-                            System.out.print("Введите марку машины: ");
-                            cars[i].carBrand = scanner.nextLine();
-                            System.out.print("Введите класс машины: ");
-                            cars[i].carClass = scanner.nextLine();
-                            System.out.print("Введите вес машины: ");
-                            cars[i].weight = scanner.nextFloat();
-                            System.out.print("Введите мощность двигателя: ");
-                            cars[i].engine.power = scanner.nextInt();
-                            scanner.nextLine();
-                            System.out.print("Введите производителя двигателя: ");
-                            cars[i].engine.maker = scanner.nextLine();
-                            i += 1;
-                        } else {
-                            System.out.println("Достигнут лимит машин. Необходимо удалить одну, прежде чем добавлять новую.");
-                        }
+                    case 1: // Добавить новую запись о автомобиле
+                        Car newCar = new Car(); // Создание нового объекта Car
+                        System.out.print("Введите ФИО водителя: ");
+                        newCar.driver.name = scanner.nextLine(); // Ввод имени водителя
+                        System.out.print("Введите стаж водителя: ");
+                        newCar.driver.driverExp = scanner.nextInt(); // Ввод стажа водителя
+                        scanner.nextLine(); // Потребление символа новой строки
+                        System.out.print("Введите марку машины: ");
+                        newCar.carBrand = scanner.nextLine(); // Ввод марки автомобиля
+                        System.out.print("Введите класс машины: ");
+                        newCar.carClass = scanner.nextLine(); // Ввод класса автомобиля
+                        System.out.print("Введите вес машины: ");
+                        newCar.weight = scanner.nextFloat(); // Ввод веса автомобиля
+                        System.out.print("Введите мощность двигателя: ");
+                        newCar.engine.power = scanner.nextInt(); // Ввод мощности двигателя
+                        scanner.nextLine(); // Потребление символа новой строки
+                        System.out.print("Введите производителя двигателя: ");
+                        newCar.engine.maker = scanner.nextLine(); // Ввод производителя двигателя
+                        carRepository.addCar(newCar); // Добавление нового автомобиля в репозиторий
                         break;
-                    case 2:
-                        Car.outputDriverInfo(i, cars);
+                    case 2: // Просмотр всех записей
+                        Car.outputAllDrivers(carRepository.getCars());
                         break;
-                    case 3:
-                        Car.outputDriverThan5Years(i, cars);
+                    case 3: // Просмотр водителей со стажем более 5 лет
+                        Car.outputDriverThan5Years(carRepository.getCars());
                         break;
-                    case 4:
-                        Car.outputDriverSovietCar(i, cars);
+                    case 4: // Просмотр водителей с советскими автомобилями
+                        Car.outputDriverSovietCar(carRepository.getCars());
                         break;
-                    case 5:
+                    case 5: // Удаление записи о автомобиле
+                        System.out.print("Введите индекс записи для удаления: ");
+                        int indexToRemove = scanner.nextInt(); // Ввод индекса для удаления
+                        carRepository.removeCar(indexToRemove); // Удаление автомобиля из репозитория
+                        break;
+                    case 6: // Выход из программы
                         System.out.println("Выход...");
                         break;
                     default:
-                        System.out.println("Неверный выбор. Пожалуйста, выберите от 1 до 5.");
+                        System.out.println("Неверный выбор. Пожалуйста, выберите от 1 до 6.");
                 }
             } catch (InputMismatchException e) {
-                System.out.println("Пожалуйста, введите число.");
-                scanner.nextLine();
+                System.out.println("Пожалуйста, введите число."); // Обработка неверного ввода
+                scanner.nextLine(); // Очистка неверного ввода
             }
-        } while (choice != 5);
+        } while (choice != 6); // Цикл продолжается, пока пользователь не выберет выход
 
-        scanner.close();
+        scanner.close(); // Закрытие сканера
     }
 }
--- lab3/src/Main.java
+++ lab3/src/Main.java
@@ -1,72 +1,90 @@
 import java.util.Scanner;
 
-class arrayProcessor {
-    int[] array;
+// Класс для обработки массива
+class ArrayProcessor {
+    int[] array; // Массив целых чисел
 
-    arrayProcessor(int n) {
+    // Конструктор для инициализации массива заданного размера
+    ArrayProcessor(int n) {
         this.array = new int[n];
     }
 
+    // Вывод элементов массива
     void outputArray(int n) {
         for (int i = 0; i < n; i++) {
             System.out.printf("%d ", array[i]);
         }
+        System.out.println(); // Переход на новую строку после вывода
     }
 
+    // Сортировка массива методом пузырька
     void sortArray() {
         int temp;
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length-i-1; j++) {
-                if (array[j] > array[j+1]) {
+            for (int j = 0; j < array.length - i - 1; j++) { // * Упрощено выражение
+                // Сравнение соседних элементов
+                if (array[j] > array[j + 1]) {
+                    // Обмен значениями
                     temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
             }
         }
     }
 
-    int binarySearch(int i, int n) {
-        int low, high, middle;
-        low = 0;
-        high = n - 1;
+    // Двоичный поиск элемента в отсортированном массиве
+    int binarySearch(int target) { //Упрощён параметр
+        int low = 0;
+        int high = array.length - 1;
         while (low <= high) {
-            middle = (low + high) / 2;
-            if (i < array[middle]) {
+            int middle = (low + high) / 2; // * Объявление переменной в цикле
+            if (target < array[middle]) {
                 high = middle - 1;
-            } else if (i > array[middle]) {
+            } else if (target > array[middle]) {
                 low = middle + 1;
             } else {
-                return middle;
+                return middle; // Элемент найден
             }
         }
-        return -1;
+        return -1; // Элемент не найден
     }
 }
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Ввод размера массива
         System.out.print("Введите размер массива: ");
         int n = scanner.nextInt();
 
-        arrayProcessor process = new arrayProcessor(n);
+        ArrayProcessor process = new ArrayProcessor(n); // * Изменено название класса
         System.out.print("Введите элементы массива: ");
+
+        // Ввод элементов массива
         for (int i = 0; i < n; i++) {
             process.array[i] = scanner.nextInt();
         }
-        process.sortArray();
+
+        process.sortArray(); // Сортировка массива
+
+        // Ввод числа для поиска
         System.out.print("Введите число для поиска в массиве: ");
-        int i = scanner.nextInt();
-        int foundInd = process.binarySearch(i, n);
-        if (foundInd != -1) {
-            System.out.printf("Индекс искомого числа в массиве: %d\n", foundInd);
+        int target = scanner.nextInt(); // * Переименован параметр для ясности
+
+        // Поиск элемента в массиве
+        int foundIndex = process.binarySearch(target);
+        if (foundIndex != -1) {
+            System.out.printf("Индекс искомого числа в массиве: %d\n", foundIndex);
         } else {
             System.out.print("Данного числа нет в массиве\n");
         }
-        System.out.print("Вывод самого массива с сортировкой: ");
+
+        // Вывод отсортированного массива
+        System.out.print("Вывод массива с сортировкой: ");
         process.outputArray(n);
+
+        scanner.close(); // Закрытие сканера
     }
 }
